079ba6eba487c83f60cd6e89cfb008fa98d57aa8
Devansh Bajaj <devansh21db.db@gmail.com>
2021-12-10 19:35:36 +0530

    init: Simple React app to fetch GET request

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  async function getData() {
+    let response = await fetch("https://jsonplaceholder.typicode.com/users");
+    let data = await response.json();
+    setUsers(data)
+  }
+  console.log(users)
+
+
+  return (
+    <div className="App">
+      <div className='container'>
+        {users.map((user) => {
+          return (
+            <div className='card' key={user.id}>
+              <p>Name: {user.name}</p>
+              <p>Username: {user.username}</p>
+              <p>Phone: {user.phone}</p>
+              <p>E-mail: {user.email}</p>
+              <p>Website: <a href={user.website} rel="noopener">{user.website}</a></p>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default App
